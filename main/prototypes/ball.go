56b3a1a73ec75743ed531f85a8ab3922f51a9570
Miguel Lopez V <miguellova@gmail.com>
2023-01-02 14:32:15 -0600

    16 - ball trail effect

--- main/prototypes/ball.go
+++ main/prototypes/ball.go
@@ -13,6 +13,41 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "trail_maker"
+  component: "/hyper_trails/trail_maker.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "trail_width"
+    value: "30.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "trail_tint_color"
+    value: "1.0, 0.3, 0.38, 0.2"
+    type: PROPERTY_TYPE_VECTOR4
+  }
+  properties {
+    id: "points_count"
+    value: "64.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "shrink_tail_width"
+    value: "true"
+    type: PROPERTY_TYPE_BOOLEAN
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
@@ -90,6 +125,30 @@ embedded_components {
   "pan: 0.0\n"
   "speed: 1.0\n"
   "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "trail_model"
+  type: "model"
+  data: "mesh: \"/hyper_trails/models/trail_64.dae\"\n"
+  "material: \"/hyper_trails/materials/trail.material\"\n"
+  "textures: \"/hyper_trails/textures/data/texture0_1.png\"\n"
+  "textures: \"/hyper_trails/textures/white.png\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
   ""
   position {
     x: 0.0
